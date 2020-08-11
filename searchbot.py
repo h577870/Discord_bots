059d18e1f3d2ef2d77d5b7f4a9d3272444ba53a1
h577870 <kristoffer.davidsen@gmail.com>
2020-08-11 20:02:43 +0200

    added cljs.core searches

--- searchbot.py
+++ searchbot.py
@@ -1,6 +1,5 @@
 import os
 from discord.ext import commands
-
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -26,8 +25,8 @@ async def on_member_join(member):
 @bot.command(name='help')
 async def helper(ctx):
 	help_commands = [
-		f'!so -> Søk etter spørsmål på Stack Overflow, f.eks. !so "Sort list python"',
-		f'!hs -> Bruk for å søke etter dokumentasjon for Haskell, f.eks. !hs map'
+		f'!so -> Søk etter spørsmål på Stack Overflow, f.eks. !so "Sort list python" ',
+		f'!hs -> Bruk for å søke etter dokumentasjon for Haskell, f.eks. !hs map '
 	]
 	response = help_commands
 	await ctx.send(response)
@@ -48,6 +47,14 @@ async def stackOverflow(ctx, keyword: str):
 	else:
 		placeholder: str = keyword.replace(' ', '+')
 		response = f'Søk etter "{keyword}" ga følgende resultater -> <https://stackoverflow.com/search?q={placeholder}>'
+	await ctx.send(response)
+
+@bot.command(name='cljs')
+async def clojureScript(ctx, keyword: str):
+	if not keyword:
+		response = 'Søk etter spørsmål i clojuredocs (foreløpig kun clojure.core) -> <https://clojuredocs.org/>'
+	else:
+		response = f'Søk etter {keyword} ga følgende resultater -> <https://clojuredocs.org/clojure.core/{keyword}>'
 	await ctx.send(response)
 
 bot.run(TOKEN)
